002d2d877d4ffb0f536b86eafbafb1bdfd921138
Sebastian <sebast.velo@gmail.com>
2021-06-11 23:46:56 -0300

    Updates

---
+++ precache-manifest.7351b2d0f09dbaf02129fb39c1197d28.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ab0da366e1e6d5dd29413129f0b42e",
+    "url": "/SebastianVelo/resume-builder-fe/index.html"
+  },
+  {
+    "revision": "7495b5bb412830b3cd2a",
+    "url": "/SebastianVelo/resume-builder-fe/static/css/main.a3a8bc6a.chunk.css"
+  },
+  {
+    "revision": "655932f0484a21f2778e",
+    "url": "/SebastianVelo/resume-builder-fe/static/js/2.a140b96b.chunk.js"
+  },
+  {
+    "revision": "c0cd05482c2c076b6ca3754f53f53494",
+    "url": "/SebastianVelo/resume-builder-fe/static/js/2.a140b96b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7495b5bb412830b3cd2a",
+    "url": "/SebastianVelo/resume-builder-fe/static/js/main.904a69b7.chunk.js"
+  },
+  {
+    "revision": "9597c6ce713c12d8e6e1",
+    "url": "/SebastianVelo/resume-builder-fe/static/js/runtime-main.c26b75f2.js"
+  },
+  {
+    "revision": "58395202fadf1210342c6bf7a4fdfa8c",
+    "url": "/SebastianVelo/resume-builder-fe/static/media/template0.58395202.png"
+  },
+  {
+    "revision": "f0f9e719d95f48dbf34a472eb1bc2f0c",
+    "url": "/SebastianVelo/resume-builder-fe/static/media/template1.f0f9e719.png"
+  },
+  {
+    "revision": "1334055d0dbf916d2bc2acf898cb8c01",
+    "url": "/SebastianVelo/resume-builder-fe/static/media/template2.1334055d.png"
+  },
+  {
+    "revision": "532d1e40b38b1568cbc5f932267262c4",
+    "url": "/SebastianVelo/resume-builder-fe/static/media/template3.532d1e40.png"
+  }
+]);
